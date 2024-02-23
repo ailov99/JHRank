40e1148e148208599349eb710514c1ded6a55eba
aki <rammluft@gmail.com>
2024-02-23 14:34:49 +0200

    Solved Matrix Layer Rotation

--- src/HardModule.jl
+++ src/HardModule.jl
@@ -39,7 +39,68 @@ function matrixRotation(
     # (use transpose as we want vectors to be rows)
     input_m = vcat([x' for x in matrix]...)
 
-    return input_m
+    # Get matrix dimensions
+    matrix_height = size(input_m)[1]
+    matrix_width = size(input_m)[2]
+    output_m = Matrix{Int}(undef, matrix_height, matrix_width)
+    
+    # We will rotate ring-by-ring
+    rings_in_matrix = min(floor(Int, matrix_height/2), floor(Int, matrix_width/2))
+    ring_width = matrix_width
+    ring_height = matrix_height
+
+    for ring = 1:rings_in_matrix
+        ring_length = 2*ring_height + 2*ring_width - 4
+        
+        # Find the offset for element (1,1) within the ring post-rotation
+        # and shift all elements using that
+        offset = r % ring_length
+
+        # Map input matrix ring index -> Output matrix ring index
+        in_ring_to_out_ring = (offset, in_ring_index) -> begin
+            # Apply offset and handle wrap-rounds (backwards)
+            offset_index = in_ring_index - offset
+            (offset_index > 0) && return offset_index
+            return ring_length + offset_index
+        end
+
+        # Map matrix ring index -> Matrix coordinates
+        ring_to_matrix_coords = (i) -> begin
+            # Need to find out where in the ring `i` lies 
+            # (first/last matrix row or first last/matrix col)
+            offset_within_matrix = ring - 1
+            if i <= ring_width
+                # i is on first row
+                return (1 + offset_within_matrix, i + offset_within_matrix)
+            elseif i <= (ring_width + ring_height - 2)
+                # i is on last col
+                offset_within_col = i - ring_width + 1
+                return (offset_within_col + offset_within_matrix, matrix_width - offset_within_matrix)
+            elseif i <= (2*ring_width + ring_height - 2)
+                # i is on last row
+                offset_within_row = (2*ring_width + ring_height - 2) - i + 1
+                return (matrix_height - offset_within_matrix, offset_within_row + offset_within_matrix)
+            else 
+                # i is on first col
+                offset_within_col = ring_length - i + 2
+                return (offset_within_col + offset_within_matrix, 1+offset_within_matrix)
+            end
+        end
+        
+        # Copy elements across matrices
+        for i = 1:ring_length  
+            out_m_i = in_ring_to_out_ring(offset, i)
+            in_m_row, in_m_col = ring_to_matrix_coords(i)
+            out_m_row, out_m_col = ring_to_matrix_coords(out_m_i)
+            output_m[out_m_row, out_m_col] = input_m[in_m_row, in_m_col]
+        end
+
+        # Shrink dimensions for next ring
+        ring_height -= 2
+        ring_width -= 2
+    end
+
+    return output_m
 end
 
 end # module HardModule
